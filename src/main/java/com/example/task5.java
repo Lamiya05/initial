9cf2692d2404cb263eb116effcd45af7d1202c33
Lamiya05 <143430252+Lamiya05@users.noreply.github.com>
2024-10-13 23:45:42 +0400

    task5.java
    OddEvenNumbers

--- src/main/java/com/example/task5.java
+++ src/main/java/com/example/task5.java
@@ -1,11 +1,33 @@
-package com.example;
+import java.util.Scanner;
 
-public class task5 {
-    public static int findOdd(int[] arr) {
-        int result = 0;
-        for (int num : arr) {
-            result ^= num;  // XOR işlemi
+public class Task3 {
+    public static void addNumbers(){
+        int a, b, c;
+        double n1;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("A= ");
+        a = input.nextInt();
+
+        System.out.print("B= ");
+        b = input.nextInt();
+
+        System.out.print("C= ");
+        c = input.nextInt();
+
+        n1 = (a + b) / c;
+
+        if (n1 < 10){
+            System.out.println("a ile b-nin ceminin c -ye bolunmesi 10-dan kicikdir");
+        } else if (n1 == 10) {
+            System.out.println("a ile b-nin ceminin c -ye bolunmesi 10-a beraberdir");
+        } else {
+            System.out.println("a ile b-nin ceminin c -ye bolunmesi 10-dan boyukdur.");
         }
-        return result;
+
+    }
+
+    public static void main(String[] args) {
+        addNumbers();
     }
 }
